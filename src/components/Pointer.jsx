b010e9e3824e0525234fad97da285a7e5082e646
20210770 <punnlert@kaist.ac.kr>
2023-12-06 04:08:32 +0900

    added pointer.jsx

---
+++ src/components/Pointer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../components/Pointer.css';
+
+function Pointer(){
+    return (
+        <div className='pointer'></div>
+    );
+}
+
+export default Pointer;
