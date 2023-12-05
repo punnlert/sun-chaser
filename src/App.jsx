4fd4ef6d8c5a0909fb2f7f305e989e126047cb8b
20210770 <punnlert@kaist.ac.kr>
2023-12-05 21:27:11 +0900

    deploy with map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import Map from './components/map'
+import Map from './components/map'
 
 function App() {
 
   return (
     <div>
       <h1>hello</h1>
-      {/* <Map/> */}
+      <Map/>
     </div>
   )
 }
